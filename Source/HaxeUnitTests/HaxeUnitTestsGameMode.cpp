de92fb0e8451be705b463f9dce3dc5197f0a4e66
Cauê Waneck <waneck@gmail.com>
2015-09-16 10:33:29 -0400

    Newly created empty project

---
+++ Source/HaxeUnitTests/HaxeUnitTestsGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HaxeUnitTests.h"
+#include "HaxeUnitTestsGameMode.h"
+
+
+
+
---
+++ Source/HaxeUnitTests/HaxeUnitTestsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HaxeUnitTestsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAXEUNITTESTS_API AHaxeUnitTestsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
