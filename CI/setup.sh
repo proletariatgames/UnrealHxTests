fc8c5730009db23ac77f39bf544c7ed9537c84c9
Cauê Waneck <waneck@gmail.com>
2015-09-16 17:05:57 -0400

    [ci] haxelib install hxml needs confirmation
    - Do not force plugin submodule update on runci.sh

--- CI/setup.sh
+++ CI/setup.sh
@@ -10,19 +10,10 @@ export HAXELIB_PATH="$PWD"/haxelib
 export CI=1
 export CI_RUNNING=1
 
+# install all needed libraries
 haxelib install hxcpp || exit $?
 haxelib install hxcs || exit $?
-# install all needed libraries
-if [ -f Haxe/arguments.hxml ]; then
-  haxelib install Haxe/arguments.hxml || exit $?
-else
-  echo "arguments.hxml doesn't exist!"
-fi
-
-# make sure we're on the very latest ue4hx version
-cd "$WORKSPACE"/Plugins/UE4Haxe
-git fetch || exit 1
-git checkout -f origin/master || exit 1
+haxelib install buddy || exit $?
 
 # build the build scripts
 cd Haxe/BuildTool
