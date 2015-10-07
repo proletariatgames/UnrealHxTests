dceb162994daf6d7ffd81b0a9a42aeb4ffbb71c5
Cauê Waneck <waneck@gmail.com>
2015-10-07 09:45:55 -0400

    Fix quotes on Windows

--- CI/runci.bat
+++ CI/runci.bat
@@ -1,6 +1,6 @@
 REM run setup and setup some env vars
 set WORKSPACE=%CD%
-SET HAXELIB_PATH="%WORKSPACE%"/haxelib
+SET HAXELIB_PATH=%WORKSPACE%/haxelib
 SET CI=1
 SET CI_RUNNING=1
 
@@ -10,7 +10,7 @@ cd %WORKSPACE%
 IF [%UE4%] == [] GOTO UE4NotDefined
 
 REM build
-cd "$UE4"
+cd "%UE4%"
 
 REM build the unit tests
 echo "building unit tests"
@@ -20,7 +20,7 @@ Engine/Build/BatchFiles/Build.bat HaxeUnitTests Win64 Development "-project=%WOR
 echo "running unit tests"
 set MAP=/Game/Maps/HaxeTestEntryPoint
 echo "%UE4%/%BINPATH% %WORKSPACE%/HaxeUnitTests.uproject -server %MAP% -log -stdout
-"$UE4"/$BINPATH "$WORKSPACE"/HaxeUnitTests.uproject -server "$MAP" -log -stdout || exit 3
+"%UE4%/%BINPATH%" "%WORKSPACE%/HaxeUnitTests.uproject" -server "%MAP%" -log -stdout || exit 3
 
 exit 0
 
