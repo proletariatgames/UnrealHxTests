ff00b7ef12a1fc6de8a0d8ae1fe1923737d1afe3
Cauê Waneck <waneck@gmail.com>
2015-10-16 10:30:16 -0400

    Don't use unity build

--- Source/HaxeUnitTests/HaxeUnitTests.Build.hx
+++ Source/HaxeUnitTests/HaxeUnitTests.Build.hx
@@ -2,21 +2,23 @@ import unrealbuildtool.*;
 
 class HaxeUnitTests extends HaxeModuleRules
 {
-	public function new(target)
-	{
-		super(target);
+  public function new(target)
+  {
+    super(target);
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+    // UEBuildConfiguration.bEnableFastIteration = true;
+    BuildConfiguration.bUseUnityBuild = false;
+    // Uncomment if you are using Slate UI
+    // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
-	}
+    // Uncomment if you are using online features
+    // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+    // if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+    // {
+    //		if (UEBuildConfiguration.bCompileSteamOSS == true)
+    //		{
+    //			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+    //		}
+    // }
+  }
 }
