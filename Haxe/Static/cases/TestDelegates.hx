bcaa504ac599d4e39f9c4cbd1f95e5e8efce0af5
Dan Ogles <danogles@gmail.com>
2015-11-12 16:09:30 -0500

    Added UObject member function callback test

--- Haxe/Static/cases/TestDelegates.hx
+++ Haxe/Static/cases/TestDelegates.hx
@@ -19,6 +19,9 @@ class FDelHaxe8 extends unreal.DynamicMulticastDelegate<Int->UUsesDelegate->Int-
 
 class FDelHaxe0_RV extends unreal.Delegate<Void->Int> {}
 class FDelHaxe1_RV extends unreal.DynamicDelegate<Int->Int> {}
+class FDelHaxeStrInt extends unreal.Delegate<FString->Int> {}
+class FDelHaxeStr_Multi extends unreal.MulticastDelegate<FString->Void> {}
+class FDelHaxe_Multi extends unreal.MulticastDelegate<Void->Void> {}
 
 @:uclass class UUsesDelegate extends unreal.UObject {
   @:uproperty(BlueprintAssignable, Category=Game)
@@ -44,6 +47,11 @@ class FDelHaxe1_RV extends unreal.DynamicDelegate<Int->Int> {}
   public var testRV:FDelHaxe1_RV;
 
   public var testCRV:FDelHaxe0_RV;
+
+  public var numCallbacks:Int = 0;
+  @:ufunction public function ufun() : Void {
+    numCallbacks++;
+  }
 }
 
 class TestDelegates extends buddy.BuddySuite {
@@ -71,12 +79,43 @@ class TestDelegates extends buddy.BuddySuite {
         del1_2.Broadcast("Hello, World!");
         value.toString().should.be("Hello, World!");
       });
-      it('should be able to unregister delegates from Haxe code');
+      it('should be able to bind to uobject functions', {
+        var del = FDelHaxe_Multi.create();
+        var obj = UObject.NewObject(new TypeParam<PStruct<UUsesDelegate>>());
+        obj.numCallbacks.should.be(0);
+        del.AddUObject(obj, MethodPointer.fromMethod(obj.ufun));
+        del.Broadcast();
+        obj.numCallbacks.should.be(1);
+      });
+      it('should be able to unregister delegates from Haxe code', {
+        var called = 0;
+        function cb(name:FString) : Void {
+          name.toString().should.be("hello");
+          ++called;
+        }
+        var del = FDelHaxeStr_Multi.create();
+        var handle = del.AddLambda(cb);
+        del.Broadcast("hello");
+        called.should.be(1);
+        del.Remove(handle);
+        del.Broadcast("world");
+        called.should.be(1);
+      });
       it('should be able to create new UCLASS types that use delegates', {
         var obj = UObject.NewObject(new TypeParam<PStruct<UUsesDelegate>>());
         obj.test0.Broadcast();
       });
-      it('should be able to declare new delegate types');
+      it('should be able to declare new delegate types', {
+        FDelHaxe0.create();
+        FDelHaxe1.create();
+        FDelHaxe2.create();
+        FDelHaxe3.create();
+        FDelHaxe4.create();
+        FDelHaxe5.create();
+        FDelHaxe6.create();
+        FDelHaxe7.create();
+        FDelHaxe8.create();
+      });
       // Delegate examples from SeekPlayerState.h
       //
       // DECLARE_DYNAMIC_MULTICAST_DELEGATE(FLootEvent);
