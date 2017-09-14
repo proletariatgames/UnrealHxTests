61d1d75cd22e807d8cb78359f78fec60e9afcc9a
Caue Waneck <waneck@gmail.com>
2017-09-13 21:42:05 -0300

    Also expand `getDefine("pass")`

--- CI/PassExpand.hx
+++ CI/PassExpand.hx
@@ -16,26 +16,35 @@ class PassExpand {
 
   public static function expand(pass:Int, file:String) {
     var contents = sys.io.File.getContent(file);
-    var regex = ~/#(?:if|elseif) *\(pass *>= *(\d+)\)/;
-    if (!regex.match(contents)) {
+    var define = ~/haxe.macro.Compiler.getDefine\("pass"\)|\/\*%PASS%\*\/"\d+"/;
+    var changed = false;
+    if (define.match(contents)) {
+      changed = true;
+      contents = define.replace(contents, '/*%PASS%*/"$pass"');
+    }
+    var regex = ~/#(if|elseif) *\(pass *>= *(\d+)\)/;
+    var didMatch = regex.match(contents);
+    if (!changed && !didMatch) {
       return;
     }
-    var changed = false;
-    var buf = new StringBuf();
-    do {
-      buf.add(regex.matchedLeft());
-      var passCheck = Std.parseInt(regex.matched(1));
-      if (pass >= passCheck) {
-        buf.add('#if true ');
-        changed = true;
-      } else {
-        buf.add(regex.matched(0));
-      }
-      contents = regex.matchedRight();
-    } while(regex.match(contents));
-    buf.add(contents);
+    var ret = contents;
+    if (didMatch) {
+      var buf = new StringBuf();
+      do {
+        buf.add(regex.matchedLeft());
+        var passCheck = Std.parseInt(regex.matched(2));
+        if (pass >= passCheck) {
+          buf.add('#${regex.matched(1)} true ');
+          changed = true;
+        } else {
+          buf.add(regex.matched(0));
+        }
+        contents = regex.matchedRight();
+      } while(regex.match(contents));
+      buf.add(contents);
+      ret = buf.toString().trim();
+    }
 
-    var ret = buf.toString().trim();
     // if (!FileSystem.exists(dest) || sys.io.File.getContent(dest).trim() != ret) {
     // if (sys.io.File.getContent(dest).trim() != ret) {
     if (changed) {
